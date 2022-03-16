4b221d0b28b277bc8d8535b7629c33ac24d7220f
jjw1270 <jjw1270@naver.com>
2022-03-16 12:27:00 +0900

    .

---
+++ template1/Assets/PlayerCtrl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public Transform mainCam;
+    public Transform firePosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        firePosition.rotation = mainCam.rotation;
+    }
+}
---
+++ template1/Assets/ShowGizmo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowGizmo : MonoBehaviour
+{
+
+    private Color color = Color.magenta;
+    private float radius = 0.5f;
+    // Start is called before the first frame update
+    void OnDrawGizmosSelected() {
+        Gizmos.color = color;
+        Gizmos.DrawSphere(this.transform.position, radius);
+    }
+}
