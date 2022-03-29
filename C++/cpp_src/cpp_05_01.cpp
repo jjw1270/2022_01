214818590f909a28e4906eb4fd16f6da196d28b1
jjw1270 <jjw1270@naver.com>
2022-03-29 17:18:00 +0900

    .

--- C++/cpp_src/cpp_05_01.cpp
+++ C++/cpp_src/cpp_05_01.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 using namespace std;
 
-void find2replace(string &fstr, string &hs, string &re, bool &success)
-{
-    for (auto &a : fstr)
-    {
-        if (a == re)
-        {
-            success = true;
-            a = re;
-        }
-    }
+class Person{
+private:
+    string name = "Anonymous";
+    int age = 0;
+    int getAge();
+public:
+    string getName();
+    //Person();
+    Person(string n, int a);
+    ~Person();
+};
+string Person::getName(){
+    return name;
+}
+int Person::getAge(){
+    return age;
+}
+Person::Person(string n, int a) : name{n}, age{a}{
+    cout << "생성자 수행 " << getName() << "," << getAge() << endl;
+}
+Person::~Person(){
+    cout << "소멸자 수행 " << getName() << endl;
 }
 
-int main()
-{
-    std::string str = "C++ programming";
-    std::string has = "+";
-    string replace = "p";
-    bool result = false;
-
-    cout << "변경 전 문자열 = " << str << endl;
-
-    find2replace(str, has, replace, result);
-
-    if (result == true)
-        cout << "변경 후 문자열 = " << str << endl;
-    else
-        cout << str << "에서 " << has << "를 발견하지 못함." << endl;
-    return 0;
+int main(){
+    Person baby;
+    Person child("benny", 10);
+    cout << "baby name = " << baby.getName() << endl;
+    cout << "child name = " << child.getName() << endl;
 }
---
+++ C++/cpp_src/cpp_05_03.cpp
@@ -0,0 +1,11 @@
+class Sample { 
+private :
+ int x;
+public:
+ int getX() const;
+};
+int main() {
+// Sample s1(4);
+ Sample s2(); 
+ Sample s3{};
+}
